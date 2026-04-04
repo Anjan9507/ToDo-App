088792f9d48d5c3f3682387998248d70af07e776
Anjan Sairaj <anjansairaj@gmail.com>
2026-04-04 20:16:58 +0530

    Implement protected routes by getting current user

---
+++ app/services/security_service.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jose import jwt, JWTError
+from app.config import SECRET_KEY, ALGORITHM
+
+security = HTTPBearer()
+
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
+    token = credentials.credentials
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        user_id = payload.get("user_id")
+        email = payload.get("email")
+
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid Token"
+            )
+        
+        return {
+            "user_id": user_id,
+            "email": email
+        }
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid or expired token"
+        )
+    except Exception as e:
+        raise e
+    
