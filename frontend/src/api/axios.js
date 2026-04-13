7fa18d8c48828590c0e1472398d7387a1d6ee403
Anjan Sairaj <anjansairaj@gmail.com>
2026-04-13 22:47:09 +0530

    Ready for frontend deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios"
 import toast from "react-hot-toast"
 
 const api = axios.create({
-	baseURL: "https://todo-backend-6b91.onrender.com",
+	baseURL: import.meta.env.VITE_API_URL,
 	withCredentials: true
 })
 
