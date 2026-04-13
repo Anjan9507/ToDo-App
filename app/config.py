b54473ab2da3aa12607fd31ecbd8fbe8e7e0e4da
Anjan Sairaj <anjansairaj@gmail.com>
2026-04-13 22:09:36 +0530

    fixed production db config

--- app/config.py
+++ app/config.py
@@ -3,12 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_HOST=os.getenv("DB_HOST")
-DB_PORT=int(os.getenv("DB_PORT", 5432))
-DB_NAME=os.getenv("DB_NAME")
-DB_USER=os.getenv("DB_USER")
-DB_PASSWORD=os.getenv("DB_PASSWORD")
-
+DB_URL=os.getenv("DB_URL")
 
 SECRET_KEY=os.getenv("SECRET_KEY")
 ALGORITHM=os.getenv("ALGORITHM")
--- app/database/db.py
+++ app/database/db.py
@@ -1,13 +1,9 @@
 from psycopg2.pool import SimpleConnectionPool
-from app.config import DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD
+from app.config import DB_URL
 
 pool = SimpleConnectionPool(
     1, 10,
-    host=DB_HOST,
-    port=DB_PORT,
-    database=DB_NAME,
-    user=DB_USER,
-    password=DB_PASSWORD
+    dsn=DB_URL
 )
 
 class DBSession:
